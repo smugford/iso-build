221da9677863be0d0174f28be00209134f94756b
smugford <scott.w.mugford@gmail.com>
2021-11-06 13:15:31 -0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,16 +1,34 @@
-#!/bin/bash
-
-sudo apt-get -qq update
-sudo apt-get -qq install -y --no-install-recommends \
-build-essential \
-curl \
-e2fsprogs \
-parted \
-qemu-utils \
-bzip2 \
-git \
-pigz \
-ansible \
-vim \
-cloud-guest-utils \
-gdisk
+!/bin/bash
+# Create Debian Base Using qemu/chroot
+# needs to be run as root
+apt update
+apt install -y qemu qemu-kvm debootstrap
+qemu-img create -f qcow2 debian.img 10G
+modprobe nbd max_part=8
+qemu-nbd --format=raw --connect=/dev/nbd0 debian.img
+sfdisk /dev/nbd0  << EOF
+1024,82
+;
+EOF
+mkswap /dev/nbd0p1
+mkfs.ext4 /dev/nbd0p2
+modinfo nbd
+mount /dev/nbd0p2 /mnt/
+debootstrap --arch=amd64 --include="openssh-server vim" stable /mnt/ http://httpredir.debian.org/debian/
+mount --bind /dev/ /mnt/dev
+sudo chroot /mnt/ /bin/sh << EOF
+mount -t proc none /proc
+mount -t sysfs none /sys
+apt install -y  linux-image-amd64 grub2
+grub-install /dev/nbd0 --force
+sudo chpasswd <<<"smugford:password"
+echo "debian" | passwd --stdin debian
+echo "pts/0" >> /etc/securetty
+systemctl set-default multi-user.target
+echo "/dev/sda2 / ext4 defaults,discard 0 0" > /etc/fstab
+umount /proc/ /sys/ /dev/
+EOF
+grub-install dev/nbd0 --root-directory=/mnt --modules="biosdisk part_msdos" --force
+sudo sed -i 's/nbd0p2/sda2/g' /mnt/boot/grub/grub.cfg
+sudo umount /mnt
+sudo qemu-nbd --disconnect /dev/nbd0
